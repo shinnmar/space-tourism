a4f0e72eb5d9b5fac78c83dc1ee8498658a22c1d
ShirleyRM <srm.shirley@gmail.com>
2024-05-18 23:55:39 -0500

    feat: add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "./components/footer/footer.component";
 import styles from "./page.module.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <div className={styles.center}></div>
+        <div className={styles.center}>
+          So, You want to travel to SPACE
+        </div>
       </main>
       <Footer />
     </>

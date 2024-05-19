50d94654e686f191294624eaf2df761560103d22
ShirleyRM <srm.shirley@gmail.com>
2024-05-19 00:54:51 -0500

    fix: add hero content

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
               <br />
               <h1 className={styles.hero_title}>SPACE</h1>
               <p className={styles.hero_paragraph}>
-                Let's face it; if you want to go to space, you might as well
-                genuinely go to outer space and not-hover kind of on the edge of
-                it. Well sit back, and relax because we'll give you a truly out
-                of this world experience!
+                Let&apos;s face it; if you want to go to space, you might as
+                well genuinely go to outer space and not-hover kind of on the
+                edge of it. Well sit back, and relax because we&apos;ll give
+                you a truly out of this world experience!
               </p>
             </article>
             <article>

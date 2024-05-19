d3aa3ae3e8c778e7b1ca750e079f6b92b8516480
ShirleyRM <srm.shirley@gmail.com>
2024-05-19 00:49:16 -0500

    feat: add hero content

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,28 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <div className={styles.center}>
-          So, You want to travel to <span>SPACE</span>
-          <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not-hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        <div className="container">
+          <div className={styles.columns}>
+            <article>
+              <p className={styles.hero_first_title}>
+                So, You want to travel to
+              </p>
+
+              <br />
+              <h1 className={styles.hero_title}>SPACE</h1>
+              <p className={styles.hero_paragraph}>
+                Let's face it; if you want to go to space, you might as well
+                genuinely go to outer space and not-hover kind of on the edge of
+                it. Well sit back, and relax because we'll give you a truly out
+                of this world experience!
+              </p>
+            </article>
+            <article>
+              <div className={styles.circle}>
+                <div className={styles.circle_text}>EXPLORE</div>
+              </div>
+            </article>
+          </div>
         </div>
       </main>
       <Footer />

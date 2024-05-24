5945efd8a3f7e181d70d3613f89f5ebda60f09a1
ShirleyRM <srm.shirley@gmail.com>
2024-05-24 00:21:02 -0500

    fix: add technology lil fix

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../technology/technology.module.scss";
+import useBodyClass from "@/hooks/useBodyClass";
 
 const Technology = () => {
+  useBodyClass("technologyPage");
   return (
     <section className={styles.technology}>
       <div>Test</div>

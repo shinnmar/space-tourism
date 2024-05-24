ef3cb73cc337b75a500731f69e3b3804e768a4e1
ShirleyRM <srm.shirley@gmail.com>
2024-05-23 23:59:36 -0500

    feat: test hook

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../crew/crew.module.scss";
+import useBodyClass from "@/hooks/useBodyClass";
 
 const Crew = () => {
+  useBodyClass("crew");
   return (
     <section className={styles.crewPage}>
       <div>Test</div>
